a692f49082faa0265420539b9c353754c021397d
Fikret CURE <fikretcure@gmail.com>
2023-12-22 19:54:26 +0300

    Updated Category.php

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  *
@@ -20,7 +21,10 @@ class Category extends Model
     ];
 
 
-    public function products()
+    /**
+     * @return HasMany
+     */
+    public function products(): HasMany
     {
         return $this->hasMany(Product::class);
     }
