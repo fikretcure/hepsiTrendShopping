906eb8837662539bf66d0abe2a85587869aa9b8b
Fikret CURE <fikretcure@gmail.com>
2023-12-22 19:53:04 +0300

    Added category and product migrations

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        Category::create([
+            'name' => 'Otomotiv'
+        ])->products()->create([
+            'name' => 'Lastik Degisimi',
+            'price' => 100,
+            'daily_stock' => 10,
+            'desc' => '1',
+            'avatar' => '1',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Category::create([
+            'name' => 'Tesisat'
+        ])->products()->create([
+            'name' => 'Musluk Tamiri',
+            'price' => 100,
+            'daily_stock' => 10,
+            'desc' => '1',
+            'avatar' => '1',
+        ]);
+
+        Category::create([
+            'name' => 'Ozel Ders'
+        ])->products()->create([
+            'name' => 'Matematik Dersi',
+            'price' => 100,
+            'daily_stock' => 10,
+            'desc' => '1',
+            'avatar' => '1',
+        ]);
     }
 }
