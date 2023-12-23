4c6130c4f485e4424e3e0181969123a02e8c6490
Fikret CURE <fikretcure@gmail.com>
2023-12-23 23:23:07 +0300

    Updated OrderItemRepository.php

--- app/Http/Repositories/OrderItemRepository.php
+++ app/Http/Repositories/OrderItemRepository.php
@@ -30,11 +30,47 @@ class OrderItemRepository extends Repository
     /**
      * @param $order_id
      * @param $product_id
-     * @param $increment
      * @return mixed
      */
-    public function incrementQuantityWhereOrderWhereProduct($order_id, $product_id, $increment): mixed
+    public function incrementQuantityWhereOrderWhereProduct($order_id, $product_id): mixed
     {
-        return $this->model->where('order_id', $order_id)->where('product_id', $product_id)->increment('quantity', $increment);
+        return $this->model->where('order_id', $order_id)->where('product_id', $product_id)->increment('quantity');
+    }
+
+
+    /**
+     * @param $order_id
+     * @param $product_id
+     * @return mixed
+     */
+    public function decrementQuantityWhereOrderWhereProduct($order_id, $product_id): mixed
+    {
+        $data = $this->model->where('order_id', $order_id)->where('product_id', $product_id)->first();
+        if ($data->quantity == 1) {
+            return $data->delete();
+        }
+        return $this->model->where('order_id', $order_id)->where('product_id', $product_id)->decrement('quantity');
+    }
+
+
+    /**
+     * @param $order_id
+     * @param $product_id
+     * @return mixed
+     */
+    public function whereOrderWhereProduct($order_id, $product_id): mixed
+    {
+        return $this->model->where('order_id', $order_id)->where('product_id', $product_id)->exists();
+    }
+
+
+    /**
+     * @param $order_id
+     * @param $product_id
+     * @return mixed
+     */
+    public function removeProduct($order_id, $product_id): mixed
+    {
+        return $this->model->where('order_id', $order_id)->where('product_id', $product_id)->delete();
     }
 }
