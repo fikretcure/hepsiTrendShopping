9f15ef66a3ce5a2e79c1ed272a65924773d00601
Fikret CURE <fikretcure@gmail.com>
2023-12-23 22:24:52 +0300

    Updated OrderItemRepository.php

--- app/Http/Repositories/OrderItemRepository.php
+++ app/Http/Repositories/OrderItemRepository.php
@@ -25,4 +25,16 @@ class OrderItemRepository extends Repository
         $this->model = new OrderItem();
         parent::__construct($this->model);
     }
+
+
+    /**
+     * @param $order_id
+     * @param $product_id
+     * @param $increment
+     * @return mixed
+     */
+    public function incrementQuantityWhereOrderWhereProduct($order_id, $product_id, $increment): mixed
+    {
+        return $this->model->where('order_id', $order_id)->where('product_id', $product_id)->increment('quantity', $increment);
+    }
 }
