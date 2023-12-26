cb67648957bda1a976c74786ba3eb9734d2b7faa
Fikret CURE <fikretcure@gmail.com>
2023-12-26 21:01:17 +0300

    Updated Repository.php

--- app/Http/Repositories/Repository.php
+++ app/Http/Repositories/Repository.php
@@ -3,7 +3,6 @@
 namespace App\Http\Repositories;
 
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 /**
